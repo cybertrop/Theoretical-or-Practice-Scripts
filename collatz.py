7b26c7210d20a2b77b231a3663886658526ab6c0
John Doe <39686917+cybertrop@users.noreply.github.com>
2018-07-26 22:30:33 -0700

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,34 +1,27 @@
 #!/bin/python
 
 # Collatz Sequencing Script 
-# Currently in the works; almost done
-
-import sys
 
 def collatz(number):
+	" Using modulus to determine even"
+	
 	if number % 2 == 0:
-		result = number // 2 
-		print result
-		return
-	else:
-		number % 2 == 1
-		result2 = 3 * number + 1
-		print result2
-		return		
+		print(number // 2)
+		return number // 2
+	
+	"Using modulus to determine odd"
+	
+	elif number % 2 == 1:
+		result = 3 * number + 1
+		print(result)
+		return result
 
-def user_choice():
-	while True:
-		print('Please choose type an integer')
-		user_choice = int(input())
-		print ('You choose ' + str(user_choice) + ' and the result for the first function is...') 
-		result = collatz(user_choice)
-		if result == 1:
-			sys.exit(0)
+	"Below we are handling non-int exceptions" 
+	
+try:
+	n = input("Enter an integer please: ")
+	while n != 1: # We are going to loop while n is not equal to one until we reach one ; hence the Collatz sequence
+		n = collatz(int(n))
 		
-
-user_choice()
-print('Everything below is output from the first function..')
-collatz(1)
-collatz(2)
-collatz(3)
-collatz(4)
+except ValueError:
+	print("Please type an integer! ")
